803d542810b0daf429f8d9b96005492cf0c80c94
Foxsiny <mitiashkinata8@gmail.com>
2025-11-18 00:39:22 +0300

    Adding a form module. Validation of input data, hashtags, and comments.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 
 import { renderPhotos } from './render-photos.js';
 import { generatePhotoData } from './generate-photo-data.js';
-import {renderBigPicture} from './render-big-picture.js';
+import {renderBigPictureTest} from './render-big-picture-test.js';
+// import { renderBigPicture } from './render-big-picture';
+// import './upload-photo.js';
+import { initializeUploadForm } from './initialize-upload-form.js';
 
 // 1. Отрисовываем маленькие фотографии, передавая данные
 renderPhotos(generatePhotoData);
+
+initializeUploadForm();
 
 // 2. Добавляем обработчик клика на контейнер
 const galleryContainer = document.querySelector('.pictures');
@@ -24,13 +29,15 @@ galleryContainer.addEventListener('click', (e) => {
     );
     // 5. Передаем найденный объект в функцию showBigPicture
     if (selectedPhotoData) {
-      renderBigPicture(selectedPhotoData);
+      renderBigPictureTest(selectedPhotoData);
     } else {
       /* eslint-disable */
       console.error('Данные для выбранной фотографии не найдены!');
     }
   }
 });
+
+
 
 // document.querySelector(".img-upload__form").addEventListener("click", (e) => {
 //   e.preventDefault();
